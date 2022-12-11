91b846b599d30703319c18443f66632933fc11c4
Callum <84125856+callum-cheshire@users.noreply.github.com>
2022-12-11 23:02:47 +0000

    commented out app.listen

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import morgan from "morgan";
 import cors from 'cors';
 import todoRouter from "./routes/todoRouter.js";
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -12,8 +12,8 @@ app.use(cors("*"));
 
 app.use("/todos", todoRouter)
 
-app.listen(PORT, () => {
-    console.log(`Cors enabled server listening on port ${PORT}`)
-})
+// app.listen(PORT, () => {
+//     console.log(`Cors enabled server listening on port ${PORT}`)
+// })
 
 export default app
